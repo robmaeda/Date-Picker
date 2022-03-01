2db6cc024dabe1849490a573af3fa7b762df76e2
Robert Maeda <robmaeda@Roberts-MacBook-Air.local>
2022-02-28 20:49:41 -0800

    added date picker

--- App.jsx
+++ App.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
+import { addDays } from 'date-fns';
 import '/styles.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 const App = () => {
-    return (
-        <div id='app'>
-            start
-        </div>
-    )
+
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 1),
+      key: 'selection'
+    }
+  ]);
+
+  return (
+    <div id='app'>
+      <DateRangePicker
+        onChange={item => setRange([item.selection])}
+        showSelectionPreview={true}
+        moveRangeOnFirstSelection={false}
+        months={2}
+        ranges={range}
+        direction='horizontal'
+        minDate={addDays(new Date(), 0)}
+        maxDate={addDays(new Date(), 180)}
+      />
+    </div>
+
+  )
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
